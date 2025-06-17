3006a970ec212f8eba3e467a2d8b58eec9ab7ce2
Piotr Krzaczkowski <slimhannya@gmail.com>
2025-06-17 22:45:06 +0200

    feat(styles): add custom theme

--- src/app/feature/fin-track/state/budget/budget.store.ts
+++ src/app/feature/fin-track/state/budget/budget.store.ts
@@ -1,24 +1,24 @@
-import { User } from '@angular/fire/auth';
-import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
+// import { User } from '@angular/fire/auth';
+// import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
 
-interface UserState {
-  isProcessing: boolean;
-  user: User | null | undefined;
-}
+// interface UserState {
+//   isProcessing: boolean;
+//   user: User | null | undefined;
+// }
 
-export const UserStore = signalStore(
-  { providedIn: 'root' },
-  withState<UserState>({
-    user: null,
-    isProcessing: false,
-  }),
-  withMethods((store) => ({
-    setUser(user: User | null) {
-      patchState(store, () => ({ user }));
-    },
+// export const UserStore = signalStore(
+//   { providedIn: 'root' },
+//   withState<UserState>({
+//     user: null,
+//     isProcessing: false,
+//   }),
+//   withMethods((store) => ({
+//     setUser(user: User | null) {
+//       patchState(store, () => ({ user }));
+//     },
 
-    setIsProcessing(isProcessing: boolean) {
-      patchState(store, () => ({ isProcessing }));
-    },
-  }))
-);
+//     setIsProcessing(isProcessing: boolean) {
+//       patchState(store, () => ({ isProcessing }));
+//     },
+//   }))
+// );
